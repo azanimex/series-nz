5deff9936d1adfd9e1787232c2ed5570c800329f
azanimex <44275977+azanimex@users.noreply.github.com>
2024-07-08 20:38:31 -0300

    Update FourOhFour.tsx

--- src/components/FourOhFour.tsx
+++ src/components/FourOhFour.tsx
@@ -9,10 +9,8 @@ const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
       </div>
       <div className="mx-auto mt-6 max-w-xl text-gray-500">
         <div className="mb-8 text-xl font-bold">
-          <Trans>
           El servidor llegó a su límite de cuota diaria. Tendrás que esperar hasta las 21hs UTC-3 para su reinicio o ir al servidor de respaldo mediante 
-          <a className="text-blue-600 hover:text-blue-700 hover:underline" href="https://series-nz2.az-animex.com/es/" target="_blank" rel="noopener noreferrer">este enlace</a>.
-          </Trans>
+            <a className="text-blue-600 hover:text-blue-700 hover:underline" href="https://series-nz2.az-animex.com/es/" target="_blank" rel="noopener noreferrer"> este enlace</a>.
         </div>
       </div>
     </div>
